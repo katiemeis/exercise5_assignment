ba136ba0e709d251c7d9dea7c429cbd7133656e2
Stephanie Araki <saraki@nd.edu>
2018-09-28 14:46:54 -0400

    added incomplete command for task 2

--- assignmentscript.sh
+++ assignmentscript.sh
@@ -2,3 +2,7 @@
 cat wages.csv | cut -d ,1,2 | sed 's/,/ /g' | sort -k1,1d -k2,2n | uniq  > gender-yearsExperience.csv 
 
 #for task 2:
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k3 | head -n 1 tail -n 1
+
+#incomplete part of task 2: will give top 10 earners and count number of lines with female
+head -n 10 | grep -n "female"
