2676cb29726d863daf2525f5a12b6ccfa660db04
Stephanie Araki <saraki@nd.edu>
2018-09-28 14:34:56 -0400

    added uniq command

--- assignmentscript.sh
+++ assignmentscript.sh
@@ -1,4 +1,4 @@
 #for task 1: sort wages.csv by gender and yearsExperience
-cat wages.csv | cut -d ,1,2 | sed 's/,/ /g' | sort -k1,1d -k2,2n > gender-yearsExperience.csv 
+cat wages.csv | cut -d ,1,2 | sed 's/,/ /g' | sort -k1,1d -k2,2n | uniq  > gender-yearsExperience.csv 
 
 #for task 2:
