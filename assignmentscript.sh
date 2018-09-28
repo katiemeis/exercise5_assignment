365e6d0c6fd944e8307a3cc26bd1ab06c6fa796d
Stephanie Araki <saraki@nd.edu>
2018-09-28 14:33:03 -0400

    added comments to script re: task 1

--- assignmentscript.sh
+++ assignmentscript.sh
@@ -1,1 +1,4 @@
-cat wages.csv | cut -d ,1,2 | sed 's/,/ /g' | sort -k1,1d -k2,2n | less -S
+#for task 1: sort wages.csv by gender and yearsExperience
+cat wages.csv | cut -d ,1,2 | sed 's/,/ /g' | sort -k1,1d -k2,2n > gender-yearsExperience.csv 
+
+#for task 2:
