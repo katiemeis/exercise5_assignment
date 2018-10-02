fab5e4e190b487a69099bfa6e615ea742c57fab0
Leigh C Campbell <lcampbe3@remote201.helios.nd.edu>
2018-10-02 18:26:16 -0400

    changed extenstion

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#bash script.sh wages.csv 
+
+cat $1 | cut -d , -f 1,2 | tr ',' ' ' | sort -t ' ' -k1,1 -k2,2 -n > $1_unique.csv
+
+echo "highest earner"
+cat $1 | grep -v 'gender' | sort -t , -k 4 -n |tail -n 1 
+
+echo "lowest earner"
+cat $1 | grep -v 'gender' | sort -t , -k 4 -n | head -n 1
+
+echo "top ten female earners"
+cat $1 | grep -w 'female' | sort -t , -k 4 -n | tail -n 10
+
+echo "effect of graduating college on minimum wage"
+VAR1=$(cat $1 | grep -E '[female|male][,][0-9]{1,4}[,]16' | sort -t , -k 4 -n | head -n 1 | cut -d , -f 4)
+VAR2=$(cat $1 | grep -E '[female|male][,][0-9]{1,4}[,]12' | sort -t , -k 4 -n | head -n 1 | cut -d , -f 4)
+echo "$VAR1-$VAR2" |bc
+
