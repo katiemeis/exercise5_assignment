37dc3bb8e89a31a3b0e2cf4dcfd2f5d759795449
macgraven <macgraven3@gmail.com>
2018-10-02 18:10:03 -0400

    Third commit with proper syntax and proper output

--- assignment_script_1.sh
+++ assignment_script_1.sh
@@ -1,7 +1,7 @@
 #Usage: cat $file | bash assignment_script_1.sh
-#Will do a thing
+#Will write a new file with unique gender-yearsExperience relations
 
 for i in *.csv
 do
-cat $i | cut -d , -f 1,2 | sed -E 's/\,/ /g' | sort -t " " -k1,1 -k2n,2 > output1.txt
+cat $i | tail -n +2 | cut -d , -f 1,2 | sed -E 's/\,/ /g' | sort -u -t " " -k1,1 -k2n,2 > output1.txt
 done
