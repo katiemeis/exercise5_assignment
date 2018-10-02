d6e6f92b495b566278be343dab38b159eb99813f
ElisabethCKerns <eckkerns@gmail.com>
2018-10-02 18:12:26 -0400

    Assigment 5 Number 2

---
+++ assignment_script_2.sh
@@ -0,0 +1,8 @@
+#Gives Gender,yearsExperience, and wage of highest earner
+cat wages.csv | sort -n f4 | sed -E 's/\,/\ /g' | head -n 1 | cut -d " " -f 2,3,4 >output2.txt
+ 
+#Gives Gender, yearsExperience, and wage of lowest earner
+cat wages.csv | sort -n -f4 | sed -E 's/\,/\ /g' | tail -n 1 | cut -d " " -f 2,3,4 >output3.txt
+
+#Gives number of females in top ten earners in data set 
+cat wages.csv | sort -n -f4 | sed -E 's/\,/\ /g' | head -n 10 | grep "female" | wc -l  >output4.txt
