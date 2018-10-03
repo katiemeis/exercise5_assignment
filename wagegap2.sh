904ec4fb5ff675aa6b26c85bfb5c877dc36bac86
Samuel Clarin <Samuel_Clarin@Samuels-MacBook-Pro-2.local>
2018-10-03 16:30:28 -0400

    new

--- wagegap2.sh
+++ wagegap2.sh
@@ -1,2 +1,10 @@
 # part 2 of the assignment
-#usage
+#usage: bash wagegap2.sh wages.csv
+
+
+echo "highest earner"
+grep -v "gender" $1 |sort -t , -r -k 4,4n | head -n 1
+echo "lowest earner"
+grep -v "gender" $1 | sort -t , -k 4,4n | head -n 1
+echo "top 10 females"
+grep -v "gender" $1 | sort -t , -k 1,1 -k 4,4n | head -n 10
