fe18be40b8b83504cfd2a2c0a325a251d9013b00
Samuel Clarin <Samuel_Clarin@Samuels-MacBook-Pro-2.local>
2018-10-03 16:50:00 -0400

    winning

--- wagegap2.sh
+++ wagegap2.sh
@@ -6,5 +6,5 @@ echo "highest earner"
 grep -v "gender" $1 |sort -t , -k 4,4nr | head -n 1
 echo "lowest earner"
 grep -v "gender" $1 | sort -t , -k 4,4n | head -n 1
-echo "top 10 females"
-grep -v "gender" $1 | sort -t , -k 1,1 -k 4,4nr | head -n 10
+echo "# of females in top 10"
+grep -v "gender" $1 | sort -t , -k 4,4nr | head -n 10 | grep -w "female" | wc -l
