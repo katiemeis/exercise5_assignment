25cf9779f5f0d5654c54950bea94f2e86df0e2e6
Stephanie Araki <saraki@nd.edu>
2018-10-04 01:25:30 -0400

    added incomplete command for task 3

--- assignmentscript.sh
+++ assignmentscript.sh
@@ -1,4 +1,8 @@
+#usage: bash assignmentscript.sh
+
 #for task 1: sort wages.csv by gender and yearsExperience and print unique combos to new file
+
+echo "Task 1:"
 
 cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | sort -k1,1d -k2,2n | uniq > gender-yearsExperience.csv | echo "File created with gender and yearsExperience combinations"
 
@@ -6,13 +10,21 @@ cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | sort -k1,1d -k2,2n | uniq > ge
 
 #for task 2: print highest and lowest earner and number of females in top ten earners to stdout
 
+echo "Task 2:"
+
 #prints highest earner
-cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | echo "highest earner: $(tail -n 1)"
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | echo "Highest Earner: $(tail -n 1)"
 
 #prints lowest earner
-cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | grep -v "gender" | echo "lowest earner: $(head -n 1)"
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | grep -v "gender" | echo "Lowest Earner: $(head -n 1)"
 
 #prints number of females in top ten earners
 cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | tail -n 10 | grep -n "female" | echo "$(wc -l) females in top ten earners"
 
+
+#for task 3: 
+
+echo "Task 3:"
+
+cat wages.csv | cut -d , -f 3,4 
 
